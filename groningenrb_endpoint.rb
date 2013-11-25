588fbf2ec74a87e117a128df7fc6f7d3fd8a1bb5
Peter Berkenbosch <peter@pero-ict.nl>
2013-11-25 22:47:12 +0100

    load the config 'groningenrb.dashing_key'

--- groningenrb_endpoint.rb
+++ groningenrb_endpoint.rb
@@ -8,7 +8,7 @@ class GroningenrbEndpoint < EndpointBase
   post '/dashing' do
     total_order = @message[:payload][:order][:totals][:order]
     HTTParty.post('http://dashing-groningenrb.herokuapp.com/widgets/lastorder',
-      :body => { auth_token: "da39a3ee5e6b4b0d3255bfef95601890afd80709", current: total_order.to_i }.to_json)
+      :body => { auth_token: @config["groningenrb.dashing_key"], current: total_order.to_i }.to_json)
 
     process_result 200, { message_id: @message[:message_id] }
   end
